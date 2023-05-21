5aa75b8d02a76382b6c7e2d865ac78eec1ed0945
Budabos <opiyocaroline20@gmail.com>
2023-05-21 21:43:17 +0000

    web_flask

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,25 +1,33 @@
 #!/usr/bin/python3
-"""Importing Flask to run the web app"""
+""" States and cites endpoint
+"""
+
 from flask import Flask, render_template
 from models import storage
-from models.state import State
 
 
 app = Flask(__name__)
 
 
+@app.route("/cities_by_states", strict_slashes=False)
+def cities_states_route():
+    """ Cities and states route
+    """
+    states = storage.all("State")
+    # cities = storage.all("City")
+
+    return render_template("8-cities_by_states.html", states=states)
+
+
 @app.teardown_appcontext
-def close(self):
-    """ Method to close the session """
+def tearDown(exception):
+    """ Closes storage session
+
+    Args:
+        exception (TYPE): Description
+    """
     storage.close()
 
 
-@app.route('/cities_by_states', strict_slashes=False)
-def cities_by_states():
-    """Displays a html page with states and cities"""
-    states = storage.all(State)
-    return render_template('8-cities_by_states.html', states=states)
-
-
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port="5000")
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
