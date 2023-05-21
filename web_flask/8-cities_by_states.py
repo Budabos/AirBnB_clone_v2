0b9576325ff1976eb4af574a4dd16f23080ed50e
Budabos <opiyocaroline20@gmail.com>
2023-05-21 19:56:44 +0000

    web_flask

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -4,6 +4,7 @@ The application listens on 0.0.0.0, port 5000.
 Routes:
     /cities_by_states: HTML page with a list of all states and related cities.
 """
+from models import *
 from models import storage
 from flask import Flask
 from flask import render_template
@@ -16,12 +17,12 @@ def cities_by_states():
     """Displays an HTML page with a list of all states and related cities.
     States/cities are sorted by name.
     """
-    states = storage.all("State")
+    states = storage.all("State").values()
     return render_template("8-cities_by_states.html", states=states)
 
 
 @app.teardown_appcontext
-def teardown(exc):
+def teardown(exception):
     """Remove the current SQLAlchemy session."""
     storage.close()
 
